bcd06ff92866645ea7993b0559b3f472605ad3d2
norlen <joacim.norlen@gmail.com>
2021-04-21 18:42:51 +0200

    better solution

--- autori/src/main.rs
+++ autori/src/main.rs
@@ -1,18 +1,8 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines().map(|l| l.unwrap()) {
-        let mut initials = String::new();
-        initials.push(line.as_bytes()[0] as char);
-
-        let line = &line[1..];
-        for (i, ch) in line.chars().enumerate() {
-            if ch == '-' {
-                let letter = line.as_bytes()[i + 1] as char;
-                initials.push(letter);
-            }
-        }
+    for line in io::stdin().lock().lines().map(|l| l.unwrap()) {
+        let initials: String = line.split('-').map(|s| s.as_bytes()[0] as char).collect();
         println!("{}", initials);
     }
 }
